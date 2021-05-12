03c08c3ad70d8ef30f422277cac664150471a33b
m-angelica-e <mescobarar@unal.edu.co>
2021-05-11 19:25:26 -0500

    Mi primer commit

---
+++ Home/indexHome.php
@@ -0,0 +1,5 @@
+<?php 
+	require_once "../Views/Partials/vHeaderAll.php";
+	require_once "../Views/Home/vIndexHome.php";
+	require_once "../Views/Partials/vFooter.php";
+?>
---
+++ Views/vIndexLogin.php
@@ -0,0 +1,30 @@
+<section>
+    <br>
+    <h1 class="titulo" align="center">Bienvenidos a Pay Bag</h1>
+        <p class="fontIntr">
+            <br>Somos una empresa que se preocupa por el bienestar de nuestros clientes <br><br><br></p>
+    <div class="principal">
+    <div class="introduccion">
+        <br>
+        <img class="imgLogin" src="Logo Pay Bag PNG.png">
+        <br><br><br>  
+    </div>
+    <div id="login">
+        <h3 align="center">Iniciar Sesión</h3>
+        <br>
+        <form action="Home/indexHome.php" method="post">
+            <div class="form-group">
+                <label for="usuario" class="labelLg">Usuario</label>
+                <input type="text" class="form-control" id="usuario" name="usuario">
+            </div>
+            <div class="form-group">
+                <label for="password" class="labelLg">Contraseña</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <br>
+            <button type="submit" class="btn btn-secondary">Iniciar Sesión</button>
+        </form>
+    </div>
+    </div>
+    <br>
+</section>
